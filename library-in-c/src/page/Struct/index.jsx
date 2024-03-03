7f24770843e072be5c48d1e414e248e85309ad89
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-03-03 18:51:16 +0100

    Finalizando section Struct

--- library-in-c/src/page/Struct/index.jsx
+++ library-in-c/src/page/Struct/index.jsx
@@ -19,13 +19,13 @@ const Struct = () => {
               <S.TextYellow>Person</S.TextYellow> {'{'}
             </span>
             <span>
-              <S.TextRed>char</S.TextRed> name[<S.TextRed>50</S.TextRed>]
+              <S.TextRed>&nbsp;&nbsp;&nbsp;char</S.TextRed> name[<S.TextRed>50</S.TextRed>]
             </span>
             <span>
-              <S.TextRed>int</S.TextRed> citNo;
+              <S.TextRed>&nbsp;&nbsp;&nbsp;int</S.TextRed> citNo;
             </span>
             <span>
-              <S.TextRed>float</S.TextRed> salary;
+              <S.TextRed>&nbsp;&nbsp;&nbsp;float</S.TextRed> salary;
             </span>
             <span>{'}'}</span>
           </S.Article>
@@ -71,36 +71,119 @@ const Struct = () => {
               <S.TextYellow>Person</S.TextYellow> {'{'}
             </span>
             <span>
-              <S.TextRed>char</S.TextRed> name[<S.TextRed>50</S.TextRed>]
+              <S.TextRed>&nbsp;&nbsp;&nbsp;char</S.TextRed> name[<S.TextRed>50</S.TextRed>]
             </span>
             <span>
-              <S.TextRed>int</S.TextRed> citNo;
+              <S.TextRed>&nbsp;&nbsp;&nbsp;int</S.TextRed> citNo;
             </span>
             <span>
-              <S.TextRed>float</S.TextRed> salary;
+              <S.TextRed>&nbsp;&nbsp;&nbsp;float</S.TextRed> salary;
             </span>
             <span>{'}'} person1;</span>
             <br />
             <span>
-              <S.TextRed>int</S.TextRed> <S.TextRed2>maim</S.TextRed2>
+              <S.TextRed>&nbsp;&nbsp;&nbsp;int</S.TextRed> <S.TextRed2>maim</S.TextRed2>
               {' ( )'}
               {' {'}
             </span>
             <span>
-              <S.TextYellow>strcpy</S.TextYellow>
+              <S.TextYellow>&nbsp;&nbsp;&nbsp;strcpy</S.TextYellow>
               {'('} person1.name,
               <S.TextGreen>{' "Greoge Orwell "'}</S.TextGreen>
               {' )'};
             </span>
-            <span>person1.cittNo = <S.TextRed>1984</S.TextRed>;</span>
-            <span>person1.salary = <S.TextRed>3500</S.TextRed>;</span>
+            <span>
+            &nbsp;&nbsp;&nbsp;person1.cittNo = <S.TextRed>1984</S.TextRed>;
+            </span>
+            <span>
+            &nbsp;&nbsp;&nbsp;person1.salary = <S.TextRed>3500</S.TextRed>;
+            </span>
             <br />
-            <span><S.TextYellow>printf</S.TextYellow>{'('}<S.TextGreen>{'" Name: %s\\n "'}</S.TextGreen>, person1.name{')'};</span>
-            <span><S.TextYellow>printf</S.TextYellow>{'('}<S.TextGreen>{'" Citizenship No.: %d\\n "'}</S.TextGreen>, person1.citNo{')'};</span>
-            <span><S.TextYellow>printf</S.TextYellow>{'('}<S.TextGreen>{'" Salary: %.2f "'}</S.TextGreen>, person1.salary{')'};</span>
+            <span>
+              <S.TextYellow>&nbsp;&nbsp;&nbsp;printf</S.TextYellow>
+              {'('}
+              <S.TextGreen>{'" Name: %s\\n "'}</S.TextGreen>, person1.name{')'};
+            </span>
+            <span>
+              <S.TextYellow>&nbsp;&nbsp;&nbsp;printf</S.TextYellow>
+              {'('}
+              <S.TextGreen>{'" Citizenship No.: %d\\n "'}</S.TextGreen>,
+              person1.citNo{')'};
+            </span>
+            <span>
+              <S.TextYellow>&nbsp;&nbsp;&nbsp;printf</S.TextYellow>
+              {'('}
+              <S.TextGreen>{'" Salary: %.2f "'}</S.TextGreen>, person1.salary
+              {')'};
+            </span>
             <br />
-            <span><S.TextBlue>return</S.TextBlue><S.TextRed2>{' ('}0{')'}</S.TextRed2>;</span>
+            <span>
+              <S.TextBlue>&nbsp;&nbsp;&nbsp;return</S.TextBlue>
+              <S.TextRed2>
+                {' ('}0{')'}
+              </S.TextRed2>
+              ;
+            </span>
           </S.Article>
+          <S.H2>Using typedef:</S.H2>
+          <S.P>
+            The {'`'}typedef{'`'} keyword is used to create an alias name for
+            data types, often used with structures to simplify the syntax of
+            declaring variables.
+          </S.P>
+          <S.Article>
+            <span>
+              <S.TextBlue>typedef struct </S.TextBlue>
+              <S.TextYellow>Distance</S.TextYellow> {'{'}
+            </span>
+            <span>
+              <S.TextRed>&nbsp;&nbsp;&nbsp;int</S.TextRed> feet;
+            </span>
+            <span>
+              <S.TextRed>&nbsp;&nbsp;&nbsp;float</S.TextRed> inch;
+            </span>
+            <span>{'}'} distances;</span>
+            <br />
+            <span>distances d1, d2;</span>
+          </S.Article>
+          <S.H2>Nested Structures:</S.H2>
+          <S.P>
+            It is possible to create structures within other structures.
+          </S.P>
+          <S.Article>
+            <span>
+              <S.TextBlue>struct</S.TextBlue>{' '}
+              <S.TextYellow>complex</S.TextYellow>
+              {' {'}
+            </span>
+            <span>
+              <S.TextRed>&nbsp;&nbsp;&nbsp;int</S.TextRed> imag;
+            </span>
+            <span>
+              <S.TextRed>&nbsp;&nbsp;&nbsp;float</S.TextRed> real;
+            </span>
+            <span>{'}'};</span>
+            <br />
+            <span>
+              <S.TextBlue>struct</S.TextBlue>{' '}
+              <S.TextYellow>number</S.TextYellow>
+              {' {'}
+            </span>
+            <span>
+              <S.TextBlue>&nbsp;&nbsp;&nbsp;struct</S.TextBlue>{' '}
+              <S.TextYellow>complex comp</S.TextYellow>;
+            </span>
+            <span>
+              <S.TextRed>&nbsp;&nbsp;&nbsp;int</S.TextRed> intergers;
+            </span>
+            <span>{'}'} num1, num2;</span>
+          </S.Article>
+          <S.H2>Why Use Structs in C?</S.H2>
+          <S.P>
+          Structs in C allow grouping related information under a single structure, facilitating the storage and manipulation of data. For example, when dealing with personal information, it is more efficient to have a single Person structure than several separate variables for each attribute.
+          </S.P>
+          <br />
+          <br />
         </S.sectionDescrible>
       </S.containerStruct>
     </>
