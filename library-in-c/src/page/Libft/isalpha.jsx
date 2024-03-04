b64207e725d601bc8742cbe1292819dc6a151907
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-03-04 23:57:34 +0100

    Finalizando ft_isalpha

--- library-in-c/src/page/Libft/isalpha.jsx
+++ library-in-c/src/page/Libft/isalpha.jsx
@@ -6,16 +6,16 @@ import Footer from '../../Components/Footer/index'
 const Isalpha = () => {
   return (
     <>
-      <S.DivFunction >
+      <S.DivFunction>
         <Header />
         <S.FunctionH1>ft_isalpha</S.FunctionH1>
         <S.ContainerLibftFunction>
           <S.P>
             The ft_isalpha function checks whether a given integer value i
-            represents an alphabet letter ( either uppercase or lowercase ) in C.
-            If the passed value is a letter, the function returns 1; otherwise,
-            it returns 0. It can be used to determine whether a character is
-            alphabetic in C programs.
+            represents an alphabet letter ( either uppercase or lowercase ) in
+            C. If the passed value is a letter, the function <S.TextRed2>returns 1 </S.TextRed2>; 
+            otherwise, it <S.TextRed2>returns 0</S.TextRed2> . It can be used to determine whether a
+            character is alphabetic in C programs.
           </S.P>
           <S.ContainerInfo>
             <span>
@@ -44,16 +44,18 @@ const Isalpha = () => {
           </S.ContainerInfo>
           <S.P>
             The logic of the function is quite simple. It uses a conditional
-            expression (if) to check if the value of i is within the range
+            expression <S.TextBlue>( if ) </S.TextBlue> to check if the value of i is within the range
             corresponding to alphabet characters. If i is in the range of
-            lowercase letters {"( 'a' to 'z' )"} or in the range of uppercase letters
-            {"( 'A' to 'Z' )"}, the function returns 1, indicating that the character
-            is an alphabet letter. Otherwise, it returns 0, indicating that the
-            character is not an alphabet letter.
+            lowercase letters <S.TextGreen>{"( 'a' to 'z' )"}</S.TextGreen> or
+            in the range of uppercase letters
+            <S.TextGreen>{"( 'A' to 'Z' )"}</S.TextGreen>, the function returns
+            1, indicating that the character is an alphabet letter. Otherwise,
+            it returns 0, indicating that the character is not an alphabet
+            letter.
           </S.P>
         </S.ContainerLibftFunction>
         <Footer />
-      </S.DivFunction >
+      </S.DivFunction>
     </>
   )
 }
