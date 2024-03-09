4744aa1905eccf16b3d4a3884d9e038c3136f285
tales1982 <102876696+tales1982@users.noreply.github.com>
2024-03-09 23:12:58 +0100

    alterarndo cor do codigo

--- library-in-c/src/page/Ft_strlen/index.jsx
+++ library-in-c/src/page/Ft_strlen/index.jsx
@@ -12,84 +12,81 @@ const Ft_strlen = () => {
         The ft_strlen function is a custom implementation of the standard strlen
         function from the C library. {"It's"} designed to calculate the length
         of a string, meaning it counts the number of characters in the string
-        until it encounters the null termination character {"('\\0')"}, which
-        indicates the end of the string in C. This function is not part of the
-        standard C library but follows a common pattern seen in custom
-        implementations or educational projects, such as those by the 42 school.
+        until it encounters the <S.Gold>null</S.Gold> termination character{' '}
+        <S.Gold>{"('\\0')"}</S.Gold>, which indicates the end of the string in
+        C. This function is not part of the standard C library but follows a
+        common pattern seen in custom implementations or educational projects,
+        such as those by the 42 school.
       </S.P>
-      <S.P>{"Let's "}break down how it works, step by step:</S.P>
-      <S.P>
-        1°<strong> Initialization: </strong>The function begins by initializing
-        a local variable i to 0. This variable is used to count the characters
-        in the string.
-      </S.P>
-      <S.P>
-        2° <strong>Loop:</strong> Next, it enters a while loop that continues as
-        long as str[i] is not zero. In other words, the loop iterates over each
-        character in the string, one by one, until the null termination
-        character is found.
-      </S.P>
-      <S.P>
-        3° <strong>Increment: </strong>Inside the loop, i is incremented in each
-        iteration. This effectively moves the index i along the string, counting
-        each character.
-      </S.P>
-      <S.P>
-        4° <strong>Return: </strong>When the loop finds the null termination
-        character, it ends, and the function returns the current value of i,
-        which represents the number of characters in the string (excluding the
-        null termination character).
-      </S.P>
-      <S.P>
-        <strong></strong>
-      </S.P>
-      <S.H3>{"Here's "}The signature of the strlen function is:</S.H3>
-      <S.SectionInfo>
-        <span>
-          <S.TextRed>int</S.TextRed> <S.TextRed2>ft_strlen</S.TextRed2>(
-          <S.TextRed> const char </S.TextRed> *str );
-        </span>
-      </S.SectionInfo>
-      <br />
+
       <S.H3>{"Here's"} an example of how the function could be used:</S.H3>
       <S.SectionInfo>
         <span>
-          # <S.TextBlue>include </S.TextBlue>{' '}
-          <S.TextGreen>{'"libft.h"'}</S.TextGreen>{' '}
+           <S.Red># include </S.Red>{' '}
+          <S.Green>{'"libft.h"'}</S.Green>{' '}
         </span>
         <br />
         <span>
-          <S.TextRed>int</S.TextRed> <S.TextRed2>ft_strlen</S.TextRed2>(
-          <S.TextRed> const char </S.TextRed> *str )
+           int <S.Lilas>ft_strlen</S.Lilas>(
+           const char <S.Green>*</S.Green>str )
         </span>
         <span>{'{'}</span>
         <S.SpanTab>
-          <S.TextRed>int </S.TextRed> <S.SpanTab>i </S.SpanTab>;
+           int   <S.SpanTab>i </S.SpanTab>;
         </S.SpanTab>
         <br />
         <S.SpanTab> i = 0 ;</S.SpanTab>
         <S.SpanTab>
-          <S.TextBlue>while </S.TextBlue> ( str[ i ] )
+          <S.Red>while </S.Red> ( str[ i ] )
         </S.SpanTab>
         <S.SpanTab>
           <S.SpanTab2>
-            i <S.TextBlue>++</S.TextBlue> ;
+            i <S.Green>++</S.Green> ;
           </S.SpanTab2>
         </S.SpanTab>
 
         <S.SpanTab>
-          <S.TextBlue>return</S.TextBlue> (<S.TextRed2> i </S.TextRed2>);
+          <S.Red>return</S.Red> (  i  );
         </S.SpanTab>
         <span>{'}'}</span>
-
       </S.SectionInfo>
       <br />
       <S.P>
-          In this example, the strlen function calculates the length of the
-          string {'"Hello, World!"'}, which is 13 characters, and then the
-          program prints this length.
-        </S.P>
-        <S.P>This implementation is quite straightforward and efficient for determining the length of strings in C, following one of the fundamental principles of programming in C, which is to work directly with pointers and character arrays for string manipulation.</S.P>
+        ° <strong> Initialization: </strong>The function begins by initializing
+        a local variable <S.Gold>i </S.Gold>to <S.Gold>0</S.Gold>. This variable
+        is used to count the characters in the string.
+      </S.P>
+      <S.P>
+        ° <strong>Loop:</strong> Next, it enters a while loop that continues as
+        long as <S.Gold>str[i]</S.Gold> is not zero. In other words, the loop
+        iterates over each character in the string, one by one, until the{' '}
+        <S.Gold>null </S.Gold>termination character is found.
+      </S.P>
+      <S.P>
+        ° <strong>Increment: </strong>Inside the loop, i is incremented in each
+        iteration. This effectively moves the index i along the string, counting
+        each character.
+      </S.P>
+      <S.P>
+        ° <strong>Return: </strong>When the loop finds the <S.Gold>null</S.Gold>{' '}
+        termination character, it ends, and the function returns the current
+        value of <S.Gold>i</S.Gold>, which represents the number of characters
+        in the string (excluding the null termination character).
+      </S.P>
+      <S.P>
+        <strong></strong>
+      </S.P>
+      <S.P>
+        ° In this example, the strlen function calculates the length of the
+        string <S.Gold>{'"Hello, World!"'}</S.Gold>, which is <S.Gold>13 </S.Gold>characters, and then the program
+        prints this length.
+      </S.P>
+      <S.P>
+        This implementation is quite straightforward and efficient for
+        determining the length of strings in C, following one of the fundamental
+        principles of programming in C, which is to work directly with pointers
+        and character arrays for string manipulation.
+      </S.P>
       <Footer />
     </S.ContainerMain>
   )
